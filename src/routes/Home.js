7d4d4933065f4c04c0d27db3b77e1b453ed279e5
Mellang <dlwpgh23@naver.com>
2021-08-29 00:00:32 +0900

    스타일링중..

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,17 +2,52 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Home = () => {
+const Home = ({ test }) => {
+  console.log(test.items.item[0]);
   return (
     <MainWrap>
-      <section>여긴 컨텐츠</section>
+      <section className="contents">
+        여긴 컨텐츠
+        <div className="card-field">
+          <div className="card">
+            <div className="card-name">
+              지금 코로나는{test.items.item[0].stdDay._text}
+            </div>
+            <div className="card-body">
+              <div className="card-chart"></div>
+              <div className="뭐넣지"></div>
+            </div>
+          </div>
+        </div>
+      </section>
     </MainWrap>
   );
 };
 
 const MainWrap = styled.div`
   padding-top: 3.625rem;
-  padding-left: 15rem;
+  .contents {
+    padding-left: 15rem;
+    background-color: beige;
+    height: 93.6vh;
+  }
+  .card-field {
+    color: black;
+    padding-left: 4rem;
+    padding-right: 4rem;
+  }
+  .card {
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
+  .card-name {
+    background-color: #f7f7f7;
+  }
+  .card-chart {
+    height: 25rem;
+  }
 `;
 
 export default Home;
--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Link, Route } from "react-router-dom";
 import styled from "styled-components";
 import NavBar from "../components/NavBar";
@@ -7,21 +7,22 @@ import SideNavBar from "../components/SideNavBar";
 import Home from "./Home";
 
 const Main = () => {
+  const [test, setTest] = useState();
   const getData = async () => {
-    let data = await axios.get(
-      `http://localhost:4000/api`
-    );
-    console.log(data);
+    let data = await axios.get(`http://localhost:4000/api`);
+    console.log(data.data.response.body);
+    setTest(data.data.response.body);
   };
   useEffect(() => {
     getData();
-  });
+    return () => {};
+  }, []);
   return (
     <BrowserRouter>
       <NavBar />
       <MainWrap>
         <SideNavBar />
-        <Route path={"/"} exact component={Home} />
+        <Route path={"/"} exact render={() => <Home test={test} />} />
       </MainWrap>
     </BrowserRouter>
   );
