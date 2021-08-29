185a07a8ec64514d4ec7f8eda116515bc4f83b8e
Mellang <dlwpgh23@naver.com>
2021-08-29 16:24:14 +0900

    그래프

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,23 +1,88 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { Line, Bar } from "react-chartjs-2";
+import axios from "axios";
 
 const Home = ({ test }) => {
-  console.log(test.items.item[0]);
+  // console.log(test.items.item[0]);
   return (
     <MainWrap>
       <section className="contents">
-        여긴 컨텐츠
-        <div className="card-field">
-          <div className="card">
-            <div className="card-name">
-              지금 코로나는{test.items.item[0].stdDay._text}
-            </div>
-            <div className="card-body">
-              <div className="card-chart"></div>
-              <div className="뭐넣지"></div>
-            </div>
+        <div
+          className="home-head"
+          style={{ marginBottom: "4rem", backgroundColor: "green" }}
+        >
+          <div>
+            <h1 style={{ marginTop: "0" }}>ㅎㅇ</h1>
           </div>
+          <div>더미</div>
+        </div>
+        <div className="cd-field">
+          <section className="cd-top">
+            <div className="cd-name">
+              최근 일주일의 코로나
+              {/*지금 코로나는 {test.items.item[0].stdDay._text} */}
+            </div>
+            <div className="cd-body">
+              <div className="cd-chart">
+                <Bar
+                  data={data}
+                  height={100}
+                  options={{
+                    maintainAspectRatio: false,
+                  }}
+                />
+              </div>
+              <div className="cd-chart-footer">아아아</div>
+            </div>
+          </section>
+          <section className="cd-bt-row">
+            <div className="cd-bt">
+              <div className="cd-padd">
+                <div className="cd-contents">
+                  <div className="cd-name">1</div>
+                  <div className="cd-bt-body">1234명(+2354)</div>
+                  <div className="cd-footer">
+                    <div>버튼</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="cd-bt">
+              <div className="cd-padd">
+                <div className="cd-contents">
+                  <div className="cd-name">2</div>
+                  <div className="cd-bt-body">1234명(+2354)</div>
+                  <div className="cd-footer">
+                    <div>버튼</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="cd-bt">
+              <div className="cd-padd">
+                <div className="cd-contents">
+                  <div className="cd-name">3</div>
+                  <div className="cd-bt-body">1234명(+2354)</div>
+                  <div className="cd-footer">
+                    <div>버튼</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="cd-bt">
+              <div className="cd-padd">
+                <div className="cd-contents">
+                  <div className="cd-name">4</div>
+                  <div className="cd-bt-body">1232234명(+2354)</div>
+                  <div className="cd-footer">
+                    <div>버튼</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
       </section>
     </MainWrap>
@@ -28,26 +93,98 @@ const MainWrap = styled.div`
   padding-top: 3.625rem;
   .contents {
     padding-left: 15rem;
-    background-color: beige;
-    height: 93.6vh;
+    background-color: #afd48d;
   }
-  .card-field {
+  .cd-field {
     color: black;
     padding-left: 4rem;
     padding-right: 4rem;
   }
-  .card {
+  .cd-top {
     background-color: white;
     display: flex;
     flex-direction: column;
     width: 100%;
+    border: 1px solid white;
+    border-radius: 0.35rem;
+    box-shadow: 0 0.15rem 1.75rem 0 rgb(34 39 46 / 15%);
   }
-  .card-name {
+  .cd-name {
     background-color: #f7f7f7;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    padding-left: 2rem;
+    padding-right: 2rem;
+    border-radius: 0.35rem 0.35rem 0 0;
   }
-  .card-chart {
+  .cd-chart {
     height: 25rem;
   }
+  .cd-chart-footer {
+    background-color: #f7f7f7;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    padding-left: 2rem;
+    padding-right: 2rem;
+    border-radius: 0 0 0.35rem 0.35rem;
+  }
+  .cd-bt-row {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 3rem;
+  }
+  .cd-bt {
+    width: 25%;
+  }
+  .cd-padd {
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+  .cd-contents {
+    display: flex;
+    flex-direction: column;
+    height: 15rem;
+    background-color: white;
+    border: 1px solid white;
+    border-radius: 0.35rem;
+    box-shadow: 0 0.15rem 1.75rem 0 rgb(34 39 46 / 15%);
+  }
+  .cd-bt-body {
+    font-size: 2.5rem;
+    text-align: center;
+    padding-top: 2.5rem;
+    padding-bottom: 2.5rem;
+  }
+  .cd-footer {
+  }
 `;
-
+const data = {
+  labels: ["그제", "어제", "현재", "현재", "현재", "현재", "현재"],
+  datasets: [
+    {
+      label: "확진자",
+      backgroundColor: "rgba(255,99,132,0.2)",
+      borderColor: "rgba(255,99,132,1)",
+      hoverBackgroundColor: "rgba(255,99,132,0.4)",
+      hoverBorderColor: "rgba(255,99,132,1)",
+      data: [65, 59, 80, 81, 56, 55, 40],
+    },
+    {
+      label: "병실환자",
+      backgroundColor: "rgba(255,99,132,1)",
+      borderColor: "rgba(255,99,132,1)",
+      hoverBackgroundColor: "rgba(255,99,132,0.4)",
+      hoverBorderColor: "rgba(255,99,132,1)",
+      data: [23, 15, 56, 77, 98, 23, 111],
+    },
+    {
+      label: "사망자",
+      backgroundColor: "rgba(255,99,132,0.2)",
+      borderColor: "rgba(255,99,132,1)",
+      hoverBackgroundColor: "rgba(255,99,132,0.4)",
+      hoverBorderColor: "rgba(255,99,132,1)",
+      data: [2, 33, 23, 12, 54, 67, 19],
+    },
+  ],
+};
 export default Home;
--- src/routes/Main.js
+++ src/routes/Main.js
@@ -14,7 +14,6 @@ const Main = () => {
     setTest(data.data.response.body);
   };
   useEffect(() => {
-    getData();
     return () => {};
   }, []);
   return (
